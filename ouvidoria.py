df98d1df311185a2d21960ae7b3c236d6ef82b12
Jessy <132463939+WOOB0T@users.noreply.github.com>
2023-06-20 12:44:46 -0300

    Restante do código ouvidoria, faltando apenas acrescentar a opção 6 do menu, "Alterar"

--- ouvidoria.py
+++ ouvidoria.py
@@ -59,7 +59,7 @@ while escolha != 8:
 
         print("\n" + redirect.center(55, " "))
 
-    if escolha == 2:
+    elif escolha == 2:
         print()
         print("-", "Lista: Categorias de Manifestações", "-")
         print("[1] Reclamações")
@@ -88,3 +88,162 @@ while escolha != 8:
             print("\n" + error.center(55, " "))
 
         print("\n" + redirect.center(55, " "))
+
+    elif escolha == 3:
+        print("\n" + "◆ Categorias de Manifestações:" + "\n")
+        print("[ 1 ] Reclamações")
+        print("[ 2 ] Sugestões")
+        print("[ 3 ] Elogios")
+
+        type = int(input("\n" + "▶ Digite pelo código em qual categoria você deseja cadastrar sua manifestação: "))
+
+        if 0 < type < 4:
+            title = input("\n" + "▶ Escreva um título que resuma sua manifestação:  ")
+            description = input("▶ Por favor, descreva sua manifestação: ")
+            nome = input("▶ Por fim, informe seu nome completo: ")
+
+            sqlInsercao = 'insert into ocorrencias (tipo, titulo , descricao , autor) values (%s,%s,%s,%s)'
+            valores = [str(type), title, description, nome]
+
+            insertNoBancoDados(conexao, sqlInsercao, valores)
+            print("\n" + "✔  Sua manifestação foi cadastrada com sucesso!")
+
+        else:
+            print("\n" + error.center(55, " "))
+
+        print("\n" + redirect.center(55, " "))
+
+    elif escolha == 4:
+        ConsultaQuantidadeTotal = "select count(*) from ocorrencias"
+        resultadoTotal = listarBancoDados(conexao, ConsultaQuantidadeTotal)
+
+        consultaQuantidadeRec = "select count(*) from ocorrencias where tipo = 1"
+        resultadoRec = listarBancoDados(conexao, consultaQuantidadeRec)
+
+        consultaQuantidadeSug = "select count(*) from ocorrencias where tipo = 2"
+        resultadoSug = listarBancoDados(conexao, consultaQuantidadeSug)
+
+        consultaQuantidadeElo = "select count(*) from ocorrencias where tipo = 3"
+        resultadoElo = listarBancoDados(conexao, consultaQuantidadeElo)
+
+        print("\n" + "◆ No total, existem", resultadoTotal[0][0], "manifestações cadastradas. Dentre essas:")
+        print("➥", resultadoRec[0][0], "são reclamações")
+        print("➥", resultadoSug[0][0], "são sugestões")
+        print("➥", resultadoElo[0][0], "são elogios")
+
+    elif escolha == 5:
+        codigo = int(input("\n" + "▶ Digite o protocolo da manifestação que deseja encontrar: "))
+
+        consultaProtocol = 'select * from ocorrencias where protocolo = ' + str(codigo)
+        ocorrenciaPesquisada = listarBancoDados(conexao, consultaProtocol)
+
+        if len(ocorrenciaPesquisada) == 1:
+            print("\n" + "⌕ A manifestação pesquisada foi:")
+            for items in ocorrenciaPesquisada:
+                print("\n" + "Categoria:", items[1], "\n" + "➥ Título:", items[2],
+                      "\n" + "➥ Descrição:", items[3], "\n" + "➥ Autor:", items[4])
+
+        else:
+            print("\n" + error.center(55, " "))
+
+    elif escolha == 6:
+        print("A adicionar.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elif escolha == 7:
+        consultaOcorrencias = "select * from ocorrencias"
+        ocorrencias = listarBancoDados(conexao, consultaOcorrencias)
+        
+        if len(ocorrencias) == 0:
+            print("\n" + "◆ Ainda não existem manifestações que possam ser deletadas!")
+
+        else:
+            print("\n" + "◆ Opções disponiveis:" + "\n")
+            print("[ 1 ] Remover Todas as Manifestações")
+            print("[ 2 ] Remover Todas as Manifestações de uma Categoria")
+            print("[ 3 ] Remover Manifestações Específicas")
+
+            remove = int(input("\n" + "▶ Digite a opção que deseja realizar: "))
+
+            if remove == 1:
+                sqlDelet = "delete from ocorrencias"
+                delAllBancoDados(conexao, sqlDelet)
+
+                print("\n" + "✔  Todas as ocorrências foram removidas com sucesso!")
+
+            elif remove == 2:
+                print("\n" + "◆ Categorias de Manifestações:" + "\n")
+                print("[ 1 ] Reclamações")
+                print("[ 2 ] Sugestões")
+                print("[ 3 ] Elogios")
+
+                codigo = int(input("\n" + "▶ Digite o código da categoria que você deseja realizar a remoção: "))
+
+                sqlDelet = "delete ocorrencias where tipo = %s "
+                dados = (str(codigo),)
+                excluirBancoDados(conexao, sqlDelet, dados)
+
+                print("\n" + "✔  Todas as manifestações da categoria selecionada foram removidas com sucesso!")
+
+            elif remove == 3:
+                print("\n" + "◆ Manifestações cadastradas:")
+                for items in ocorrencias:
+                    print("\n" + "Protocolo:", items[0], "-", "Categoria:", items[1], "\n" + "➥ Título:", items[2])
+
+                codigo = input("\n" + "▶ Digite o protocolo da manifestação que você deseja deletar: ")
+
+                consultaProtocol = 'select * from ocorrencias where protocolo = ' + str(codigo)
+                ocorrenciaPesquisada = listarBancoDados(conexao, consultaProtocol)
+
+                if len(ocorrenciaPesquisada) == 1:
+                    sqlDelet = "delete from ocorrencias where protocolo = %s "
+                    dados = (codigo,)
+                    excluirBancoDados(conexao, sqlDelet, dados)
+
+                    print("\n" + "✔  A ocorrencia de protocolo", codigo, "foi removida com sucesso!")
+
+                else:
+                    print("\n" + error.center(55, " "))
+            else:
+                print("\n" + error.center(55, " "))
+
+        print("\n" + redirect.center(55, " "))
+
+    elif escolha == 8:
+        print("\n" + "Agradecemos por ter utilizado o nosso sistema da Ouvidoria, "
+                     "a opinião dos nossos associados é muito importante para nós!")
+
+    else:
+        print("\n" + error.center(55, " "))
+        print("\n" + redirect.center(55, " "))
+
+encerrarBancoDados(conexao)
