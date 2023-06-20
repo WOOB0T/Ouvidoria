6d6ad2f069262a6cbf407608bd309fa6c8ec33f9
unknown <thaysebarros61@gmail.com>
2023-06-20 12:04:08 -0300

    Incluindo a listagem por categoria

--- ouvidoria.py
+++ ouvidoria.py
@@ -50,10 +50,41 @@ while escolha != 8:
 
         else:
             print("\n" + "◆ Manifestações cadastradas até o momento:")
+
             for items in ocorrencias:
                 print("\n" + "Protocolo:", items[0], "-", "Categoria:", items[1], "\n" + "➥ Título:", items[2],
-                      "\n" + "➥ Descrição:", items[3], "\n" + "➥ Autor:", items[4])
-        
+                  "\n" + "➥ Descrição:", items[3], "\n" + "➥ Autor:", items[4])
+
             print("\n" + "◆ Fim da listagem.")
+
+        print("\n" + redirect.center(55, " "))
+
+    if escolha == 2:
+        print()
+        print("-", "Lista: Categorias de Manifestações", "-")
+        print("[1] Reclamações")
+        print("[2] Sugestões")
+        print("[3] Elogios")
+        print()
+
+        categoria = int(input("▶ Digite a categoria da manifestação que deseja listar:"))
+
+        if 0 < categoria < 4:
+
+            listagemTipo = "select * from ocorrencias where tipo =" + str(categoria)
+            ocorrenciasTipo = listarBancoDados(conexao, listagemTipo)
+
+            if len(ocorrenciasTipo) == 0:
+                print("Ainda não existem manifestações nessa categoria!")
+
+            else:
+                print('-', "Manifestações cadastradas até o momento:")
+                for o in ocorrenciasTipo:
+                    print("Protocolo: ", o[0], "/", "Título:", o[2], "/", "Autor:", o[4])
+
+                print("\n" + "◆ Fim da listagem.")
+
+        else:
+            print("\n" + error.center(55, " "))
 
         print("\n" + redirect.center(55, " "))
